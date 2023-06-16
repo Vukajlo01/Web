d75dcd703ec1c0e2c3c266c29f951d79043078ee
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-06-16 12:58:30 +0200

    Order class

---
+++ WebProject/Models/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Order
+    {
+        public Product product { get; set; }
+        public int Amount { get; set; }
+        //public User Buyer { get; set; }
+        //public DateTime OrderingDate { get; set; }
+        //public Status Status { get; set; }
+    }
+}
