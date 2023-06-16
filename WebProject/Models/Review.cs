d209d58ab7325886ba3d1b7adffadefc9ee39ef2
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-06-16 13:02:48 +0200

    Enums added

---
+++ WebProject/Models/Review.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Review
+    {
+        public Product Product { get; set; }
+        //public User ReviewAuthor { get; set; }
+        public string Title { get; set; }
+        //public string ReviewContent { get; set; }
+        ////public ??? Image { get; set; }
+    }
+}
---
+++ WebProject/Models/Status.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public enum Status
+    {
+        ACTIVE,
+        DELIVERED,
+        CANCELLED
+    }
+}
