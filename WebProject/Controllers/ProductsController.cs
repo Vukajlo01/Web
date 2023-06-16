e6f614c2f7665c77863d938c06c88355cfa4696f
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-06-16 13:07:53 +0200

    ProductsController

---
+++ WebProject/Controllers/ProductsController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using WebProject.Models;
+
+namespace WebProject.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        public List<Product> Get()
+        {
+            return Products.listOfProducts;
+        }
+    }
+}
