5ce3e0dde4ae68c4d55825a42f2073055964caa7
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-06-16 13:03:27 +0200

    User class

---
+++ WebProject/Models/User.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class User
+    {
+        [Key]
+        public Guid Id { get; set; }
+        public string Username { get; set; } //unique
+        public string Password { get; set; }
+        //public string FirstName { get; set; }
+        //public string LastName { get; set; }
+        //public string Gender { get; set; }
+        //public string Email { get; set; }
+        //public DateTime Birthdate { get; set; } //dd/MM/yyyy
+        public Role Role { get; set; }
+        //public List<Order> Orders { get; set; } //only if role = Buyer
+        //public List<Product> FavProducts { get; set; } //only if role = Buyer
+        //public List<Product> PublishedProducts { get; set; } //only if role = Prodavac
+
+    }
+}
