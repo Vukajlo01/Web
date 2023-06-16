c19a6d42f0b0a2d268220b5a29bf1f430ea147d1
Vuk Ognjanovic <56590132+Vukajlo01@users.noreply.github.com>
2023-06-16 12:59:23 +0200

    Product class

---
+++ WebProject/Models/Product.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace WebProject.Models
+{
+    public class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+        public int Amount { get; set; }
+
+        public Product(string name, double price, int amount)
+        {
+            Name = name;
+            Price = price;
+            Amount = amount;
+        }
+        //public string Description { get; set; }
+        ////public ??? Image { get; set; }
+        ////public DateTime PublishingDate { get; set; } //dd/MM/yyyy
+        //public string City { get; set; }
+        //public List<Review> reviews { get; set; }
+        //public bool Available { get; set; }
+
+
+    }
+}
